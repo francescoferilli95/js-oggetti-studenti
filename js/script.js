fcbcba1c6a880a53732d4ed8e2b145768102d7a4
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-04-13 14:09:49 +0200

    first task completed

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ *  TASKS:
+ * 
+ *  1. CREATE AN OBJECT WHO DESCRIBES A STUDENT, USE THREE PROPERTIES: NAME, SURNAME, AGE; LOG THE PROPERTIES OF THIS OBJECT.
+ *  2. CREATE AN ARRAY OF OBJECTS ABOUT STUDENTS AND LOG NAME AND SURNAME.
+ *  3. ASK THE USER TO INSERT THREE INFO, (NAME, SURNAME, AGE), CREATE A NEW OBJECT WITH THESE INFO AND ADD THIS OBJECT TO THE PREVIOUS ARRAY OF OBJECTS.
+ * 
+/**/
+
+
+/**
+ * 
+ *  TASK 1
+ * 
+/**/
+
+// CREATING AN OBJECT
+/*/
+var student = {
+    "name": 'Francesco',
+    surname: 'Ferilli',
+    age: '25',
+};
+// LOG THE OBJECT
+// console.log(student);
+
+
+// LOOP TO LOG ALL THE OBJECT'S PROPERTIES
+for( key in student) {
+    console.log(student[key]);
+}
+*/
+
+
+/**
+ * 
+ *  TASK 2
+ * 
+/**/
+
